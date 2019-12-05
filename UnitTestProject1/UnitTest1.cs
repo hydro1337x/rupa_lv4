169b517cd37f6e50e6bf66f6cb914d6793afaeeb
Benjamin Mecanović <benjamin.mecanovic@gmail.com>
2019-12-05 19:08:36 +0100

    First commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class CheckShoppingCartQuantityCounter
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "http://demowebshop.tricentis.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheCheckUserRegistrationWithANumberAsFristAndLastNameTest()
+        {
+            driver.Navigate().GoToUrl("http://demowebshop.tricentis.com/");
+            driver.FindElement(By.LinkText("Register")).Click();
+            driver.FindElement(By.Id("FirstName")).Click();
+            driver.FindElement(By.Id("FirstName")).Clear();
+            driver.FindElement(By.Id("FirstName")).SendKeys("101");
+            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Your Personal Details'])[1]/following::div[1]")).Click();
+            driver.FindElement(By.Id("LastName")).Click();
+            driver.FindElement(By.Id("LastName")).Clear();
+            driver.FindElement(By.Id("LastName")).SendKeys("101");
+            driver.FindElement(By.Id("Email")).Click();
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("101@gmail.com");
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("10101010");
+            driver.FindElement(By.Id("ConfirmPassword")).Click();
+            driver.FindElement(By.Id("ConfirmPassword")).Clear();
+            driver.FindElement(By.Id("ConfirmPassword")).SendKeys("10101010");
+            driver.FindElement(By.Id("register-button")).Click();
+        }
+
+        [Test]
+        public void TheCheckLoginWithValidInformationRegisteredUserInformationTest()
+        {
+            driver.Navigate().GoToUrl("http://demowebshop.tricentis.com/");
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("Email")).Click();
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("101@gmail.com");
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("10101010");
+            driver.FindElement(By.XPath("//input[@value='Log in']")).Click();
+        }
+
+        [Test]
+        public void TheCheckShoppingCartQuantityCounterTest()
+        {
+            driver.Navigate().GoToUrl("http://demowebshop.tricentis.com/cart");
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("Email")).Click();
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("101@gmail.com");
+            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Email:'])[1]/following::label[1]")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("10101010");
+            driver.FindElement(By.XPath("//input[@value='Log in']")).Click();
+            driver.FindElement(By.LinkText("Books")).Click();
+            driver.FindElement(By.XPath("//input[@value='Add to cart']")).Click();
+            driver.FindElement(By.LinkText("Log out")).Click();
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("Email")).Click();
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("101@gmail.com");
+            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Returning Customer'])[1]/following::form[1]")).Click();
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("10101010");
+            driver.FindElement(By.XPath("//input[@value='Log in']")).Click();
+        }
+
+
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
+
